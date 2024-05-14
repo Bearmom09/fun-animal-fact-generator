13fdcfa05dd9659c3861b85c71e092083a6c61d0
Bearmom09 <160854050+Bearmom09@users.noreply.github.com>
2024-05-14 11:04:42 +0200

    Added basic form handeling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateFact(event) {
+  event.preventDefault();
 
+  new Typewriter("#fun-fact", {
+    strings: "bla bla bla bla bla bla bla bla bla bla bla bla",
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let factFormElement = document.querySelector("#fact-generator");
+factFormElement.addEventListener("submit", generateFact);
