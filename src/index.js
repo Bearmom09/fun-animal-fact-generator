b908c507c25df988d6b51ed6354b43cb28f1afc7
Bearmom09 <160854050+Bearmom09@users.noreply.github.com>
2024-05-14 11:51:04 +0200

    Added AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateFact(event) {
-  event.preventDefault();
-
+function displayFunFact(response) {
   new Typewriter("#fun-fact", {
-    strings: "bla bla bla bla bla bla bla bla bla bla bla bla",
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
     cursor: "",
   });
+}
+
+function generateFact(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "ta4fa299a6f9403bo6f20f088f5ac04c";
+  let prompt = `Please generate a fun-fact about ${instructionsInput.value}`;
+  let context =
+    "You are an expert on animals. please display your one best fun-fact in basic HTML and make sure to seperate each line with a <br />. Please sign the fun-fact with `SheCodes AI` inside a <strong> element after the fun-fact";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayFunFact);
 }
 
 let factFormElement = document.querySelector("#fact-generator");
