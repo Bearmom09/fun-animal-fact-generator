bd157636036e9528e435bc2b4f4d25cba9dfa59b
Bearmom09 <160854050+Bearmom09@users.noreply.github.com>
2024-05-15 13:05:09 +0200

    improved the UX

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ function generateFact(event) {
   let apiKey = "ta4fa299a6f9403bo6f20f088f5ac04c";
   let prompt = `Please generate a fun-fact about ${instructionsInput.value}`;
   let context =
-    "You are an expert on animals. please display your one best fun-fact in basic HTML and make sure to seperate each line with a <br />. Please sign the fun-fact with `SheCodes AI` inside a <strong> element after the fun-fact";
+    "You are an expert on animals. please display your one best fun-fact in basic HTML but do not write that you are presenting it in basic HTML. Make sure to seperate each line with a <br />. Please sign the fun-fact with `SheCodes AI` inside a <strong> element after the fun-fact on a seperate line";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let funFactElement = document.querySelector("#fun-fact");
+  funFactElement.classList.remove("hidden");
+  funFactElement.innerHTML = `Generating a fun-fact about ${instructionsInput.value}, please wait`;
 
   axios.get(apiURL).then(displayFunFact);
 }
